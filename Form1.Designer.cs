da189cfd16f6bd9e08092eff9c72d5483c729869
nerm1k <nerm1k@mail.ru>
2024-03-21 16:41:43 +0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace wheelOfFortune
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(233, 64);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(486, 486);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(428, 2);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(60, 56);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(747, 317);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 50);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(744, 133);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(22, 152);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "1";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(103, 152);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "2";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(22, 181);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "5";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(103, 181);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "10";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(22, 210);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "20";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(103, 210);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 9;
+            this.button7.Text = "Д";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Ставка на х1: ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(22, 111);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 11;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Ставка на х2: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(19, 38);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Ставка на х5: ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(19, 51);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(84, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Ставка на х10: ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(19, 64);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(84, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Ставка на х20: ";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(19, 77);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(84, 13);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Ставка на х40: ";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(791, 38);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(35, 13);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "label8";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(930, 543);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Resources;
+
+
+namespace wheelOfFortune
+{
+    public partial class Form1 : Form
+    {
+        private bool wheelIsMoved;
+        private float wheelTimes;
+        private Timer wheelTimer;
+        private WheelOfFortune wheel;
+
+        private int bet_x1 = 0;
+        private int bet_x2 = 0;
+        private int bet_x5 = 0;
+        private int bet_x10 = 0;
+        private int bet_x20 = 0;
+        private int bet_x40 = 0;
+
+        private Dictionary<int, int> bets;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            wheelTimer = new Timer();
+            wheelTimer.Interval = 50; // как часто вызывается метод ниже
+            wheelTimer.Tick += wheelTimer_Tick;
+            wheel = new WheelOfFortune();
+
+        }
+
+        public class WheelOfFortune
+        {
+            public Bitmap wheelPic;
+            public Bitmap tempWheelPic;
+            public float angle;
+            public int[] states;
+            public int state;
+
+            public WheelOfFortune()
+            {
+                tempWheelPic = new Bitmap(Properties.Resources.wheel);
+                wheelPic = new Bitmap(Properties.Resources.wheel);
+                states = new int[] { 1, 2, 20, 1, 5, 2, 1, 10, 1, 2, 1, 5, 1, 40, 1, 2, 1, 2, 1, 5, 1, 10, 1, 2, 1, 2, 1, 5, 1, 20, 1, 2, 1, 2, 1, 10, 2, 5, 1, 2, 40, 2, 1, 2, 1, 5, 1, 2, 1, 10, 1, 5, 1, 2 };
+                angle = 0.0f;
+            }
+
+            public Bitmap RotateImage(Image image, float angle, PointF offset = default(PointF))
+            {
+                if (image == null)
+                {
+                    throw new ArgumentNullException("image");
+                }
+
+                Bitmap rotatedBmp = new Bitmap(image.Width, image.Height);
+                rotatedBmp.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+                Graphics g = Graphics.FromImage(rotatedBmp);
+
+                if (offset == default(PointF))
+                {
+                    offset = new PointF((float)image.Width / 2, (float)image.Height / 2);
+                }
+
+                g.TranslateTransform(offset.X, offset.Y);
+                g.RotateTransform(angle);
+                g.TranslateTransform(-offset.X, -offset.Y);
+                g.DrawImage(image, new PointF(0, 0));
+
+                return rotatedBmp;
+            }
+        }
+
+
+        private void wheelTimer_Tick(object sender, EventArgs e)
+        {
+            if (wheelIsMoved && wheelTimes > 0)
+            {
+                wheel.angle += wheelTimes / 10;
+                wheel.angle = wheel.angle % 360;
+                Bitmap rotatedImage = wheel.RotateImage(wheel.wheelPic, wheel.angle);
+                pictureBox1.Image = rotatedImage;
+                wheelTimes--;
+
+                wheel.state = Convert.ToInt32(Math.Ceiling(wheel.angle / 6.666666666666667));
+
+                if (wheel.state == 0)
+                {
+                    wheel.state = 0;
+                }
+                else
+                {
+                    wheel.state -= 1;
+                }
+
+
+                label1.Text = Convert.ToString(wheel.states[wheel.state]);
+            }
+            else
+            {
+                wheelIsMoved = false;
+                int result = wheel.states[wheel.state];
+
+                foreach(var bet in bets)
+                {
+                    if (bet.Key == result)
+                    {
+                        label8.Text = "Вы выиграли: " + bet.Value * result;
+                    }
+                }
+
+                bet_x1 = 0;
+                bet_x2 = 0;
+                bet_x5 = 0;
+                bet_x10 = 0;
+                bet_x20 = 0;
+                bet_x40 = 0;
+
+                label2.Text = "Ставка на х1: ";
+                label3.Text = "Ставка на х2: ";
+                label4.Text = "Ставка на х5: ";
+                label5.Text = "Ставка на х10: ";
+                label6.Text = "Ставка на х20: ";
+                label7.Text = "Ставка на х40: ";
+
+                bets.Clear();
+            }
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            wheelIsMoved = true;
+            Random rand = new Random();
+            wheelTimes = rand.Next(50, 100); // кол-во кручений
+            wheelTimer.Start();
+            bets = new Dictionary<int, int>
+            {
+                {1, bet_x1},
+                {2, bet_x2},
+                {5, bet_x5},
+                {10, bet_x10},
+                {20, bet_x20},
+                {40, bet_x40}
+            };
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char button = e.KeyChar;
+
+            if (!Char.IsDigit(button) && button != (char)8)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(textBox1.Text))
+            {
+                bet_x1 += int.Parse(textBox1.Text);
+                label2.Text = "Ставка на х1: " + bet_x1;
+                textBox1.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Please enter a number in the TextBox.");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(textBox1.Text))
+            {
+                bet_x2 += int.Parse(textBox1.Text);
+                label3.Text = "Ставка на х2: " + bet_x2;
+                textBox1.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Please enter a number in the TextBox.");
+            }
+        }
+
+      
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(textBox1.Text))
+            {
+                bet_x5 += int.Parse(textBox1.Text);
+                label4.Text = "Ставка на х5: " + bet_x5;
+                textBox1.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Please enter a number in the TextBox.");
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(textBox1.Text))
+            {
+                bet_x10 += int.Parse(textBox1.Text);
+                label5.Text = "Ставка на х10: " + bet_x10;
+                textBox1.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Please enter a number in the TextBox.");
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(textBox1.Text))
+            {
+                bet_x20 += int.Parse(textBox1.Text);
+                label6.Text = "Ставка на х20: " + bet_x20;
+                textBox1.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Please enter a number in the TextBox.");
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(textBox1.Text))
+            {
+                bet_x40 += int.Parse(textBox1.Text);
+                label7.Text = "Ставка на х40: " + bet_x40;
+                textBox1.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Please enter a number in the TextBox.");
+            }
+        }
+    }
+
+}
